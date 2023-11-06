a6db6a7f2f9ec2c5f89eb0c0f6290f23bfc6776e
Vladimir Nadulich <g240596448@gmail.com>
2023-11-06 21:46:45 +0300

    Установщик пакета

--- installlocalhost.bat
+++ installlocalhost.bat
@@ -1,8 +1,9 @@
 @echo off
-OSPX_DIR=temp_ospx
+SET OSPX_DIR=temp_ospx
 md %OSPX_DIR% 2> nul
+del %OSPX_DIR%\*.ospx 2> nul
+call opm build -m ./packagedef -o ./%OSPX_DIR%/
+call opm install -f ./%OSPX_DIR%/*.ospx
 del %OSPX_DIR%\*.ospx
-opm build -m ./packagedef -o ./%OSPX_DIR%/
-opm install -f ./%OSPX_DIR%/*.ospx
-rd \S \Q %OSPX_DIR% 2> nul
-echo "Installation completed successfully..."
+rd %OSPX_DIR%
+echo Installation completed successfully...
